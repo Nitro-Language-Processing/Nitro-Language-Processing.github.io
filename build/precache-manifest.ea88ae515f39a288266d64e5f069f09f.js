b205f99e61d78d767f65127da3862c258ef5acb3
Livia <livia.magureanu@gmail.com>
2022-02-21 02:16:10 +0200

    current version

---
+++ build/precache-manifest.ea88ae515f39a288266d64e5f069f09f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e23b26ee09c77cb50738f6016355e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181dfa50ba5f9fedbb17",
+    "url": "/static/css/2.1eb7945e.chunk.css"
+  },
+  {
+    "revision": "7ad313b9b88045d5482c",
+    "url": "/static/css/main.6cd09185.chunk.css"
+  },
+  {
+    "revision": "181dfa50ba5f9fedbb17",
+    "url": "/static/js/2.123cff1a.chunk.js"
+  },
+  {
+    "revision": "39f97d7e64ff832133b1",
+    "url": "/static/js/3.182d6258.chunk.js"
+  },
+  {
+    "revision": "7ad313b9b88045d5482c",
+    "url": "/static/js/main.11450593.chunk.js"
+  },
+  {
+    "revision": "76fd126cbaee9811fcb3",
+    "url": "/static/js/runtime~main.4024eb39.js"
+  },
+  {
+    "revision": "e09ebc4a261ef17169886cde85c74303",
+    "url": "/static/media/bg1.e09ebc4a.jpg"
+  },
+  {
+    "revision": "bed3ac3e6f13ccb2a9cb4a27fa76f046",
+    "url": "/static/media/header.bed3ac3e.png"
+  },
+  {
+    "revision": "e1a5bc52cdfe1c731cd9ae01321e624c",
+    "url": "/static/media/lexend-mega-all-400-normal.e1a5bc52.woff"
+  },
+  {
+    "revision": "46956df68953700072e21e0ea12fb071",
+    "url": "/static/media/lexend-mega-latin-400-normal.46956df6.woff2"
+  },
+  {
+    "revision": "77555ed2a9ac3c9a00a72a1ad1ca323c",
+    "url": "/static/media/lexend-mega-latin-ext-400-normal.77555ed2.woff2"
+  },
+  {
+    "revision": "1b3b7a7921b835758bc4a5d917589d04",
+    "url": "/static/media/lexend-mega-vietnamese-400-normal.1b3b7a79.woff2"
+  },
+  {
+    "revision": "0588b4cd0c1d49d4398493f1267225b3",
+    "url": "/static/media/logo2.0588b4cd.png"
+  },
+  {
+    "revision": "c01829df421f93bdac75a7d11b0a67e6",
+    "url": "/static/media/nunito-sans-all-400-normal.c01829df.woff"
+  },
+  {
+    "revision": "bc6c701f6f78f6d4e339522f28a0c9db",
+    "url": "/static/media/nunito-sans-cyrillic-400-normal.bc6c701f.woff2"
+  },
+  {
+    "revision": "0f21627677c6988d95cc6be8ca9d7885",
+    "url": "/static/media/nunito-sans-cyrillic-ext-400-normal.0f216276.woff2"
+  },
+  {
+    "revision": "c79c009218c6e26806e0cf5be5e7d2ae",
+    "url": "/static/media/nunito-sans-latin-400-normal.c79c0092.woff2"
+  },
+  {
+    "revision": "a9c7fd95c4df27c3cc64addc17aa0bc9",
+    "url": "/static/media/nunito-sans-latin-ext-400-normal.a9c7fd95.woff2"
+  },
+  {
+    "revision": "02660d0fc0a60d1bcd375c7e454ee6e2",
+    "url": "/static/media/nunito-sans-vietnamese-400-normal.02660d0f.woff2"
+  }
+]);
