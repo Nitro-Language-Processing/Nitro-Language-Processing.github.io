4bc6257947c736ac431a41e60f7905b9350c33b0
Livia <livia.magureanu@gmail.com>
2022-03-09 11:52:24 +0200

    Updates

---
+++ precache-manifest.cf2ed883bf581d0e2568994841d65199.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cadd8a828712b628b318b28823b2d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba46ac0f51d32f816444",
+    "url": "/static/css/2.1eb7945e.chunk.css"
+  },
+  {
+    "revision": "f5ce39f72abf8ffe63ff",
+    "url": "/static/css/main.432b2f78.chunk.css"
+  },
+  {
+    "revision": "ba46ac0f51d32f816444",
+    "url": "/static/js/2.ae40b283.chunk.js"
+  },
+  {
+    "revision": "e465c9a40f34cb06cf6c",
+    "url": "/static/js/3.ecd97250.chunk.js"
+  },
+  {
+    "revision": "f5ce39f72abf8ffe63ff",
+    "url": "/static/js/main.11432480.chunk.js"
+  },
+  {
+    "revision": "629259a8c42fc99103a3",
+    "url": "/static/js/runtime~main.9251413f.js"
+  },
+  {
+    "revision": "9e7098b1b14fb95d07292e4651ba0111",
+    "url": "/static/media/Alex.9e7098b1.jpg"
+  },
+  {
+    "revision": "6162f3fea173174b35d77ff075a8ee14",
+    "url": "/static/media/Bleo.6162f3fe.jpg"
+  },
+  {
+    "revision": "6312e79836267cd36a56c33cdc823d9f",
+    "url": "/static/media/Livia.6312e798.jpg"
+  },
+  {
+    "revision": "cfda5ae90056915ac55a4978be5df952",
+    "url": "/static/media/Luci.cfda5ae9.jpg"
+  },
+  {
+    "revision": "8ead6f208a9727d9e5d8cb289c87fe92",
+    "url": "/static/media/Miruna-F.8ead6f20.jpg"
+  },
+  {
+    "revision": "9d108b455fb8d0e9d1bb07bb97c99fd0",
+    "url": "/static/media/Miruna-Z.9d108b45.jpg"
+  },
+  {
+    "revision": "ec4f2460a6fe66bf42aa370dcc736130",
+    "url": "/static/media/fundal-home-page.ec4f2460.jpg"
+  },
+  {
+    "revision": "e1a5bc52cdfe1c731cd9ae01321e624c",
+    "url": "/static/media/lexend-mega-all-400-normal.e1a5bc52.woff"
+  },
+  {
+    "revision": "46956df68953700072e21e0ea12fb071",
+    "url": "/static/media/lexend-mega-latin-400-normal.46956df6.woff2"
+  },
+  {
+    "revision": "77555ed2a9ac3c9a00a72a1ad1ca323c",
+    "url": "/static/media/lexend-mega-latin-ext-400-normal.77555ed2.woff2"
+  },
+  {
+    "revision": "1b3b7a7921b835758bc4a5d917589d04",
+    "url": "/static/media/lexend-mega-vietnamese-400-normal.1b3b7a79.woff2"
+  },
+  {
+    "revision": "aa2c7ec30f1693f4481558672ddfde1c",
+    "url": "/static/media/logo-asmi.aa2c7ec3.png"
+  },
+  {
+    "revision": "5707927f52d4d8092156b7c519f25339",
+    "url": "/static/media/logo-bitdefender.5707927f.png"
+  },
+  {
+    "revision": "8b21e68693539a403e419d746b3076bb",
+    "url": "/static/media/logo-eeml-black.8b21e686.png"
+  },
+  {
+    "revision": "a13223ae77296004510deeafa33585b1",
+    "url": "/static/media/logo-sparktech.a13223ae.png"
+  },
+  {
+    "revision": "10c9190e6af451e31395d56b6b7b3c04",
+    "url": "/static/media/logo-ub.10c9190e.png"
+  },
+  {
+    "revision": "f078499de6c6c0fad27521ae4f92c931",
+    "url": "/static/media/logo1.f078499d.png"
+  },
+  {
+    "revision": "c01829df421f93bdac75a7d11b0a67e6",
+    "url": "/static/media/nunito-sans-all-400-normal.c01829df.woff"
+  },
+  {
+    "revision": "bc6c701f6f78f6d4e339522f28a0c9db",
+    "url": "/static/media/nunito-sans-cyrillic-400-normal.bc6c701f.woff2"
+  },
+  {
+    "revision": "0f21627677c6988d95cc6be8ca9d7885",
+    "url": "/static/media/nunito-sans-cyrillic-ext-400-normal.0f216276.woff2"
+  },
+  {
+    "revision": "c79c009218c6e26806e0cf5be5e7d2ae",
+    "url": "/static/media/nunito-sans-latin-400-normal.c79c0092.woff2"
+  },
+  {
+    "revision": "a9c7fd95c4df27c3cc64addc17aa0bc9",
+    "url": "/static/media/nunito-sans-latin-ext-400-normal.a9c7fd95.woff2"
+  },
+  {
+    "revision": "02660d0fc0a60d1bcd375c7e454ee6e2",
+    "url": "/static/media/nunito-sans-vietnamese-400-normal.02660d0f.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b20f3928b69cf24d634c148a1fc6eb18.js"
+  "/precache-manifest.cf2ed883bf581d0e2568994841d65199.js"
 );
 
 self.addEventListener('message', (event) => {
