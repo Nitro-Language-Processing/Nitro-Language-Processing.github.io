b3364a2cd5416eacfe4f24badf90102a4bc6499c
Livia <livia.magureanu@gmail.com>
2022-06-03 00:42:26 +0300

    Updates

---
+++ precache-manifest.9a17de599f1f53abd9c6dd440550428e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d529a355bc705a39cdcf66147dd1372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011dca1f470b2c8aee41",
+    "url": "/static/css/2.d44bc173.chunk.css"
+  },
+  {
+    "revision": "54f1b241175419adb6f2",
+    "url": "/static/css/main.6a65646c.chunk.css"
+  },
+  {
+    "revision": "011dca1f470b2c8aee41",
+    "url": "/static/js/2.f6f4f5ef.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.f6f4f5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a5fb72e2bc27e35f13",
+    "url": "/static/js/3.7bf102e3.chunk.js"
+  },
+  {
+    "revision": "54f1b241175419adb6f2",
+    "url": "/static/js/main.b36217be.chunk.js"
+  },
+  {
+    "revision": "05cd1b6bd7107456478e",
+    "url": "/static/js/runtime-main.6d0a9af9.js"
+  },
+  {
+    "revision": "9e7098b1b14fb95d07292e4651ba0111",
+    "url": "/static/media/Alex.9e7098b1.jpg"
+  },
+  {
+    "revision": "6162f3fea173174b35d77ff075a8ee14",
+    "url": "/static/media/Bleo.6162f3fe.jpg"
+  },
+  {
+    "revision": "6312e79836267cd36a56c33cdc823d9f",
+    "url": "/static/media/Livia.6312e798.jpg"
+  },
+  {
+    "revision": "cfda5ae90056915ac55a4978be5df952",
+    "url": "/static/media/Luci-I.cfda5ae9.jpg"
+  },
+  {
+    "revision": "2471adc32f5d956f8568d3ce0d3af195",
+    "url": "/static/media/Luci-T.2471adc3.jpg"
+  },
+  {
+    "revision": "8ead6f208a9727d9e5d8cb289c87fe92",
+    "url": "/static/media/Miruna-F.8ead6f20.jpg"
+  },
+  {
+    "revision": "9d108b455fb8d0e9d1bb07bb97c99fd0",
+    "url": "/static/media/Miruna-Z.9d108b45.jpg"
+  },
+  {
+    "revision": "ec4f2460a6fe66bf42aa370dcc736130",
+    "url": "/static/media/fundal-home-page.ec4f2460.jpg"
+  },
+  {
+    "revision": "e1a5bc52cdfe1c731cd9ae01321e624c",
+    "url": "/static/media/lexend-mega-all-400-normal.e1a5bc52.woff"
+  },
+  {
+    "revision": "b261942a1952787d69fab792201329cb",
+    "url": "/static/media/lexend-mega-latin-400-normal.b261942a.woff2"
+  },
+  {
+    "revision": "dbac7f7b8100efaa16d5424cbb4d05ad",
+    "url": "/static/media/lexend-mega-latin-ext-400-normal.dbac7f7b.woff2"
+  },
+  {
+    "revision": "bee27146e70ca63480c81403356df60f",
+    "url": "/static/media/lexend-mega-vietnamese-400-normal.bee27146.woff2"
+  },
+  {
+    "revision": "77164e993da3a6254ed074c376ca9949",
+    "url": "/static/media/logo-asmi.77164e99.png"
+  },
+  {
+    "revision": "5707927f52d4d8092156b7c519f25339",
+    "url": "/static/media/logo-bitdefender.5707927f.png"
+  },
+  {
+    "revision": "8b21e68693539a403e419d746b3076bb",
+    "url": "/static/media/logo-eeml-black.8b21e686.png"
+  },
+  {
+    "revision": "0cac322620228f7c68996a474f77a6f5",
+    "url": "/static/media/logo-olr.0cac3226.png"
+  },
+  {
+    "revision": "ae13ebe5f39224a973dd6946db81a6a5",
+    "url": "/static/media/logo-ovidius.ae13ebe5.png"
+  },
+  {
+    "revision": "f3d2322a8b1074aa17207c7ae3da778a",
+    "url": "/static/media/logo-sigmoid-black.f3d2322a.png"
+  },
+  {
+    "revision": "e0dbf0830eb6e22afea7aa6a95a373f4",
+    "url": "/static/media/logo-sparktech.e0dbf083.png"
+  },
+  {
+    "revision": "10c9190e6af451e31395d56b6b7b3c04",
+    "url": "/static/media/logo-ub.10c9190e.png"
+  },
+  {
+    "revision": "f078499de6c6c0fad27521ae4f92c931",
+    "url": "/static/media/logo1.f078499d.png"
+  },
+  {
+    "revision": "9fcb97bd294f3910f343550f08d11bff",
+    "url": "/static/media/nunito-sans-all-400-normal.9fcb97bd.woff"
+  },
+  {
+    "revision": "20e47154e74db925e07ade5697e98a17",
+    "url": "/static/media/nunito-sans-cyrillic-400-normal.20e47154.woff2"
+  },
+  {
+    "revision": "e31bbed60cdd9e38b215cf968e1f7e20",
+    "url": "/static/media/nunito-sans-cyrillic-ext-400-normal.e31bbed6.woff2"
+  },
+  {
+    "revision": "8a97f720d330e75ccdbda9ae0e9f5e90",
+    "url": "/static/media/nunito-sans-latin-400-normal.8a97f720.woff2"
+  },
+  {
+    "revision": "c38f7fce769cb9e643da42c0fed6633c",
+    "url": "/static/media/nunito-sans-latin-ext-400-normal.c38f7fce.woff2"
+  },
+  {
+    "revision": "3177547fe57530a577327327a2e7cfc2",
+    "url": "/static/media/nunito-sans-vietnamese-400-normal.3177547f.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.110866b0c1276383745dfdda915fb673.js"
+  "/precache-manifest.9a17de599f1f53abd9c6dd440550428e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
