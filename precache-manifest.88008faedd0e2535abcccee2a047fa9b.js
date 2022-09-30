4986b127d486b7913318f5be604d576588e8fbf7
Livia <livia.magureanu@gmail.com>
2022-09-30 13:49:38 +0300

    Updates

---
+++ precache-manifest.88008faedd0e2535abcccee2a047fa9b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1ecf6a30c2627650bf",
+    "url": "/static/css/main.3088d258.chunk.css"
+  },
+  {
+    "revision": "7b1ecf6a30c2627650bf",
+    "url": "/static/js/main.00301e9e.chunk.js"
+  },
+  {
+    "revision": "face622145c5f80723f8",
+    "url": "/static/js/runtime~main.8823e5ba.js"
+  },
+  {
+    "revision": "49e7956087b715fc4cb6",
+    "url": "/static/css/2.a8d60d3e.chunk.css"
+  },
+  {
+    "revision": "49e7956087b715fc4cb6",
+    "url": "/static/js/2.1a92abce.chunk.js"
+  },
+  {
+    "revision": "84f7a781b6806de5124f",
+    "url": "/static/js/3.5fb5517d.chunk.js"
+  },
+  {
+    "revision": "f078499de6c6c0fad27521ae4f92c931",
+    "url": "/static/media/logo1.f078499d.png"
+  },
+  {
+    "revision": "6162f3fea173174b35d77ff075a8ee14",
+    "url": "/static/media/Bleo.6162f3fe.jpg"
+  },
+  {
+    "revision": "cfda5ae90056915ac55a4978be5df952",
+    "url": "/static/media/Luci-I.cfda5ae9.jpg"
+  },
+  {
+    "revision": "2471adc32f5d956f8568d3ce0d3af195",
+    "url": "/static/media/Luci-T.2471adc3.jpg"
+  },
+  {
+    "revision": "6312e79836267cd36a56c33cdc823d9f",
+    "url": "/static/media/Livia.6312e798.jpg"
+  },
+  {
+    "revision": "9d108b455fb8d0e9d1bb07bb97c99fd0",
+    "url": "/static/media/Miruna-Z.9d108b45.jpg"
+  },
+  {
+    "revision": "77164e993da3a6254ed074c376ca9949",
+    "url": "/static/media/logo-asmi.77164e99.png"
+  },
+  {
+    "revision": "5707927f52d4d8092156b7c519f25339",
+    "url": "/static/media/logo-bitdefender.5707927f.png"
+  },
+  {
+    "revision": "8b21e68693539a403e419d746b3076bb",
+    "url": "/static/media/logo-eeml-black.8b21e686.png"
+  },
+  {
+    "revision": "e0dbf0830eb6e22afea7aa6a95a373f4",
+    "url": "/static/media/logo-sparktech.e0dbf083.png"
+  },
+  {
+    "revision": "0cac322620228f7c68996a474f77a6f5",
+    "url": "/static/media/logo-olr.0cac3226.png"
+  },
+  {
+    "revision": "f3d2322a8b1074aa17207c7ae3da778a",
+    "url": "/static/media/logo-sigmoid-black.f3d2322a.png"
+  },
+  {
+    "revision": "ae13ebe5f39224a973dd6946db81a6a5",
+    "url": "/static/media/logo-ovidius.ae13ebe5.png"
+  },
+  {
+    "revision": "10c9190e6af451e31395d56b6b7b3c04",
+    "url": "/static/media/logo-ub.10c9190e.png"
+  },
+  {
+    "revision": "ec4f2460a6fe66bf42aa370dcc736130",
+    "url": "/static/media/fundal-home-page.ec4f2460.jpg"
+  },
+  {
+    "revision": "8ead6f208a9727d9e5d8cb289c87fe92",
+    "url": "/static/media/Miruna-F.8ead6f20.jpg"
+  },
+  {
+    "revision": "9e7098b1b14fb95d07292e4651ba0111",
+    "url": "/static/media/Alex.9e7098b1.jpg"
+  },
+  {
+    "revision": "6fd8ffa766741b5d99f3f2690e53500b",
+    "url": "/static/media/lexend-mega-vietnamese-400-normal.6fd8ffa7.woff2"
+  },
+  {
+    "revision": "2c7d3145fd5c9749b31c8d43788e5602",
+    "url": "/static/media/lexend-mega-all-400-normal.2c7d3145.woff"
+  },
+  {
+    "revision": "31477775a51b7fceb10c5099e1075282",
+    "url": "/static/media/lexend-mega-latin-ext-400-normal.31477775.woff2"
+  },
+  {
+    "revision": "c20c5ce73f88ef17f60cc97987bfea32",
+    "url": "/static/media/lexend-mega-latin-400-normal.c20c5ce7.woff2"
+  },
+  {
+    "revision": "20e47154e74db925e07ade5697e98a17",
+    "url": "/static/media/nunito-sans-cyrillic-400-normal.20e47154.woff2"
+  },
+  {
+    "revision": "e31bbed60cdd9e38b215cf968e1f7e20",
+    "url": "/static/media/nunito-sans-cyrillic-ext-400-normal.e31bbed6.woff2"
+  },
+  {
+    "revision": "3177547fe57530a577327327a2e7cfc2",
+    "url": "/static/media/nunito-sans-vietnamese-400-normal.3177547f.woff2"
+  },
+  {
+    "revision": "9fcb97bd294f3910f343550f08d11bff",
+    "url": "/static/media/nunito-sans-all-400-normal.9fcb97bd.woff"
+  },
+  {
+    "revision": "8a97f720d330e75ccdbda9ae0e9f5e90",
+    "url": "/static/media/nunito-sans-latin-400-normal.8a97f720.woff2"
+  },
+  {
+    "revision": "c38f7fce769cb9e643da42c0fed6633c",
+    "url": "/static/media/nunito-sans-latin-ext-400-normal.c38f7fce.woff2"
+  },
+  {
+    "revision": "8dab9b52e578611401188da0b89dadec",
+    "url": "/index.html"
+  }
+];
